b884aaf99b3adc96e07a66bc2001a96077705f92
dg_lennon <kbjwdh@gmail.com>
2016-06-01 17:15:47 +0800

    init

---
+++ src/reducers/index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by dg_lennon on 16/5/31.
+ */
+import {combineReducers} from 'redux-immutable';
+import {LOCATION_CHANGE} from 'react-router-redux';
+import Immutable from 'immutable';
+const initialState = Immutable.fromJS({
+  locationBeforeTransitions: null
+});
+function routerReducer(state = initialState, action){
+  if (action.type === LOCATION_CHANGE) {
+    return state.merge({
+      locationBeforeTransitions: action.payload
+    });
+  }
+
+  return state;
+}
+
+import items from './items'
+//使用redux的combineReducers方法将所有reducer打包起来
+const rootReducer = combineReducers({
+  routing:routerReducer,
+  items
+});
+
+export default rootReducer
